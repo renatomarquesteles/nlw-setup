e96befdea9f403fd3984f422190bbc9d304804c7
Renato <renatomarquesteles@gmail.com>
2023-02-23 14:51:39 -0300

    5: adds transitions and outlines

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -57,7 +57,7 @@ export const NewHabitForm = () => {
         type="text"
         id="title"
         placeholder="Ex.: Exercise, sleep well etc"
-        className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
+        className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400 focus:outline-none focus:ring-2 focus:ring-violet-500 focus:ring-offset-2 focus:ring-offset-zinc-900"
         autoFocus
         value={title}
         onChange={(event) => setTitle(event.target.value)}
@@ -71,11 +71,11 @@ export const NewHabitForm = () => {
         {availableWeekDays.map((weekDay, index) => (
           <Checkbox.Root
             key={weekDay}
-            className="flex items-center gap-3 group"
+            className="flex items-center gap-3 group focus:outline-none"
             checked={weekDays.includes(index)}
             onCheckedChange={() => handleToggleWeekDay(index)}
           >
-            <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+            <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 transition-colors group-focus:ring-2 group-focus:ring-violet-500 group-focus:ring-offset-2 group-focus:ring-offset-background">
               <Checkbox.Indicator>
                 <Check size={20} className="text-white" />
               </Checkbox.Indicator>
@@ -88,7 +88,7 @@ export const NewHabitForm = () => {
 
       <button
         type="submit"
-        className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500"
+        className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500 transition-colors focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-zinc-900"
       >
         <Check size={20} weight="bold" />
         Confirm
--- web/src/components/ProgressBar.tsx
+++ web/src/components/ProgressBar.tsx
@@ -9,7 +9,7 @@ export const ProgressBar = (props: ProgressBarProps) => {
         role="progressbar"
         aria-label="Habit progress on this day"
         aria-valuenow={props.progress}
-        className="h-3 rounded-xl bg-violet-600"
+        className="h-3 rounded-xl bg-violet-600 transition-all"
         style={{ width: `${props.progress}%` }}
       />
     </div>
